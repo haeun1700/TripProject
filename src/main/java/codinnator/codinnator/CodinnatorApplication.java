2f4ae133b6c3790ebd32027fe32444fedbd20c42
장하은 <haeun7700@hs.ac.kr>
2026-02-07 15:16:59 +0900

    initial commit

---
+++ src/main/java/codinnator/codinnator/CodinnatorApplication.java
@@ -0,0 +1,13 @@
+package codinnator.codinnator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinnatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodinnatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/codinnator/codinnator/CodinnatorApplicationTests.java
@@ -0,0 +1,13 @@
+package codinnator.codinnator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodinnatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
