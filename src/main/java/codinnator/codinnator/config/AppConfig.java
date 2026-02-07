a78b5a30f084bc5d90675a56ae6694698aef9de0
111607963 <user@codinnator.com>
2026-02-07 09:07:43 +0000

    dfgsgsfg

--- src/main/java/codinnator/codinnator/config/AppConfig.java
+++ src/main/java/codinnator/codinnator/config/AppConfig.java
@@ -5,7 +5,7 @@ import java.util.Random;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-
+asdfsadfdfsdfsaf
 @Configuration
 @ComponentScan(basePackages = "codinnator.codinnator")
 public class AppConfig {
