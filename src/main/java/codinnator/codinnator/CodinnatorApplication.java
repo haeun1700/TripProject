13650d97a2015f92e89222fe2c189278df9ead97
111607963 <user@codinnator.com>
2026-02-08 12:09:46 +0000

    test3

--- src/main/java/codinnator/codinnator/CodinnatorApplication.java
+++ src/main/java/codinnator/codinnator/CodinnatorApplication.java
@@ -3,6 +3,7 @@ package codinnator.codinnator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 // test
+//test
 @SpringBootApplication
 public class CodinnatorApplication {
 
