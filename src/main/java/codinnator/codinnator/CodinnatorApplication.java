5b7e2898c774c3f96fe12e9dd2ec7a815c74cdb1
111607963 <user@codinnator.com>
2026-02-24 19:40:31 +0900

    xc

--- src/main/java/codinnator/codinnator/CodinnatorApplication.java
+++ src/main/java/codinnator/codinnator/CodinnatorApplication.java
@@ -1,11 +1,11 @@
 package codinnator.codinnator;
-
+fffgcv
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+1sd
 @SpringBootApplication
 public class CodinnatorApplication {
-
+cd
 	public static void main(String[] args) {
 		SpringApplication.run(CodinnatorApplication.class, args);
 	}
